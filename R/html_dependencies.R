fcaa143623bea08a743a7cd8f1a902230ea32208
JJ Allaire <jj@rstudio.org>
2016-10-24 13:46:19 -0400

    move tutor.js and tutor.css

--- R/html_dependencies.R
+++ R/html_dependencies.R
@@ -3,7 +3,7 @@ tutor_html_dependency <- function() {
   htmltools::htmlDependency(
     name = "tutor",
     version = packageVersion("tutor"),
-    src = system.file("www", package = "tutor"),
+    src = system.file("www/tutor", package = "tutor"),
     script = "tutor.js",
     stylesheet = "tutor.css"
   )
