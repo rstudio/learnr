9093183a2f2ce07753c5f0d8b1878132a0a60389
JJ Allaire <jj@rstudio.org>
2016-11-06 07:41:07 -0500

    add examples to docs

--- R/quiz.R
+++ R/quiz.R
@@ -13,6 +13,25 @@
 #'   correct.
 #' @param incorrect Text to print for an incorrect answer (defaults to "Incorrect.")
 #' @param ... One or more answers 
+#' 
+#' @examples 
+#' \dontrun{
+#' question("What number is the letter A in the alphabet?",
+#'   answer("8"),
+#'   answer("14"),
+#'   answer("1", correct = TRUE),
+#'   answer("23")
+#' )
+#' 
+#' question("Where are you right now? (select ALL that apply)",
+#'   answer("Planet Earth", correct = TRUE),
+#'   answer("Pluto"),
+#'   answer("At a computing device", correct = TRUE),
+#'   answer("In the Milky Way", correct = TRUE),
+#'   incorrect = paste0("Incorrect. You're on Earth, ",
+#'                      "in the Milky Way, at a computer.")
+#' )
+#' }
 #'
 #' @name question
 #' @export
