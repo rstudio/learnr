cc05c1d9c71fb7ada6a47ca368eeb61ba28d5adb
JJ Allaire <jj@rstudio.org>
2016-11-07 09:45:12 -0500

    scaffold dependencies directory

--- tools/SlickQuiz/clone-and-add-upstream.sh
+++
@@ -1,7 +0,0 @@
-
-if [ ! -d "SlickQuiz" ]; then
-  git clone git@github.com:rstudio/SlickQuiz.git
-  (cd SlickQuiz && git remote add upstream https://github.com/jewlofthelotus/SlickQuiz.git)
-fi
-
-
--- tools/SlickQuiz/sync-upstream.sh
+++
@@ -1,6 +0,0 @@
-
-git fetch upstream
-git checkout master
-git merge upstream/master
-
-
