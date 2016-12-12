a25470dab2de28dbe594e4433d33287a51354cd9
JJ Allaire <jj@rstudio.org>
2016-12-12 17:12:44 -0500

    show check progress in toc

--- inst/rmarkdown/templates/tutorial/resources/tutorial-format.js
+++ inst/rmarkdown/templates/tutorial/resources/tutorial-format.js
@@ -2,8 +2,25 @@
 
 $(document).ready(function() {
   
-  // sign up for tutor init event
-  tutor.onInit(function() {
+  // update toc w/ section completed events
+  function showProgress(section) {
+    
+    // get the data-unique name from inside the section
+    var dataUnique = section.children('div[data-unique]');
+    if (dataUnique.length > 0) {
+       // find toc entry w/ this data-unique and add a check to it
+       dataUnique = dataUnique.attr('data-unique');
+       var tocEntry = $("#section-TOC").find('li[data-unique="' + dataUnique + '"]');
+       var check = $('<div class="pull-right"></div>');
+       var icon = $('<i class="fa fa-check"></i>');
+       check.append(icon);
+       check.append("&nbsp;");
+       tocEntry.append(check);
+    }
+  }
+  
+  // add start over button to toc
+  function addStartOverButton() {
     
     var tocControls = $('<div class="toc-controls"></div>');
     var button = $('<button class="btn btn-light btn-sm"></button>');
@@ -16,13 +33,23 @@ $(document).ready(function() {
     });
     tocControls.append(button);
     
-    
     // append controls to toc
     $("#section-TOC").append(tocControls);
+  }
+  
+
+  
+  // initialize components within tutor.onInit event
+  tutor.onInit(function() {
     
+    // show progress for section completed events
+    tutor.onProgress(function(progressEvent) {
+      if (progressEvent.event === "section_completed")
+        showProgress($(progressEvent.element));
+    });
+    
+    // add a start-over button to the TOC
+    addStartOverButton();
   });
-  
- 
-  
   
 });
