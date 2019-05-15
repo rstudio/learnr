664d81c8db2c2648911e25f2269e7bb84fe7f9f2
Barret Schloerke <schloerke@gmail.com>
2019-05-15 10:19:48 -0400

    add tests

--- tests/testthat/test-available-tutorials.R
+++ tests/testthat/test-available-tutorials.R
@@ -2,10 +2,13 @@
 context("available tutorials")
 
 test_that("Tutorial names are retrieved", {
-  
-  expect_error(available_tutorials("not a package"), "No tutorials found")
+
+  expect_error(available_tutorials("not a package"), "No package found")
+  expect_error(available_tutorials("base"), "No tutorials found")
   expect_true("hello" %in% available_tutorials("learnr"))
   expect_true("hello" %in% suppressMessages(run_tutorial(package = "learnr")))
+
+  expect_error(run_tutorial("doesn't exist", package = "learnr"), "Available ")
   expect_message(run_tutorial(package = "learnr"), "Available ")
 
 })
