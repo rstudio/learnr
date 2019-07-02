a75dc042751bfc22f83a1d37d8fe44629519fa57
Andrie <apdevries@gmail.com>
2019-07-02 19:36:13 +0100

    Document the new, fifth, method for customizing questions

--- R/question_methods.R
+++ R/question_methods.R
@@ -1,31 +1,39 @@
-
-
-#' Custom question methods
-#'
-#' There are four methods used to define a custom question.  Each s3 method should correspond to the `type = TYPE` supplied to the question.
-#'
-#' \describe{
-#'   \item{\code{question_initialize_input.TYPE(question, answer_input, ...)}}{
-#'     Determines how the question is initially displayed to the users. This should return an shiny UI object that can be displayed using \code{shiny::\link[shiny]{renderUI}}. In the case of \code{question_initialize_input.radio}, it returns a \code{shiny::\link[shiny]{radioButtons}} object. This method will be re-executed if the question is attempted again.
-#'   }
-#'   \item{question_completed_input.TYPE(question, ...)}{
-#'     Determines how the question is displayed after a submission.  Just like \code{question_initialize_input}, this method should return an shiny UI object that can be displayed using \code{shiny::\link[shiny]{renderUI}}.
-#'   }
-#'   \item{question_is_valid.TYPE(question, answer_input, ...)}{
-#'     This method should return a boolean that determines if the input answer is valid.  Depending on the value, this function enables and disables the submission button.
-#'   }
-#'   \item{question_is_correct.TYPE(question, answer_input, ...)}{
-#'     This function should return the output of \code{learnr::\link{question_is_correct_value}}.  \code{learnr::\link{question_is_correct_value}} allows for custom messages in addition to the determination of an answer being correct.  See \code{\link{question_is_correct_value}} for more details.
-#'   }
-#' }
+#' Custom question methods.
 #'
 #'
+#' @description
+#' There are five methods used to define a custom question.  Each S3 method
+#' should correspond to the `type = TYPE` supplied to the question.
 #'
-#' @param question \code{\link{question}} object used
+#' * `question_initialize_input.TYPE(question, answer_input, ...)`
+#'
+#'     -  Determines how the question is initially displayed to the users. This should return a shiny UI object that can be displayed using [shiny::renderUI]. For example, in the case of `question_initialize_input.radio`, it returns a [shiny::radioButtons] object. This method will be re-executed if the question is attempted again.
+#'
+#' * `question_completed_input.TYPE(question, ...)`
+#'
+#'     - Determines how the question is displayed after a submission.  Just like `question_initialize_input`, this method should return an shiny UI object that can be displayed using [shiny::renderUI].
+#'
+#' * `question_is_valid.TYPE(question, answer_input, ...)`
+#'
+#'      - This method should return a boolean that determines if the input answer is valid.  Depending on the value, this function enables and disables the submission button.
+#'
+#' * `question_is_correct.TYPE(question, answer_input, ...)`
+#'
+#'     - This function should return the output of [question_is_correct_value]. [question_is_correct_value] allows for custom messages in addition to the determination of an answer being correct.  See [question_is_correct_value] for more details.
+#'
+#' * `question_try_again_input <- function(question, answer_input, ...)`
+#'
+#'     - Determines how the question is displayed to the users while  the "Try again" screen is displayed.  Usually this function will disable inputs to the question, i.e. prevent the student from changing the answer options. Similar to `question_initialize_input`, this should should return a shiny UI object that can be displayed using [shiny::renderUI].
+#'
+#'
+#'
+#'
+#' @param question [question] object used
 #' @param answer_input user input value
 #' @param ... future parameter expansion and custom arguments to be used in dispatched s3 methods.
 #' @export
-#' @seealso For more information and question type extension examples, please view the \code{question_type} tutorial: \code{learnr::run_tutorial("question_type", "learnr")}.
+#' @seealso For more information and question type extension examples, please view the `question_type` tutorial: `learnr::run_tutorial("question_type", "learnr")`.
+#'
 #' @rdname question_methods
 question_initialize_input <- function(question, answer_input, ...) {
   UseMethod("question_initialize_input", question)
