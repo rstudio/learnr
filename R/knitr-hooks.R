3b230e1627b9b8c878cd1c0d7a846f0d9bf3c682
Barret Schloerke <schloerke@gmail.com>
2019-12-17 17:45:03 -0500

    use the engine provided
    I am not sold that `R` should always be displayed

--- R/knitr-hooks.R
+++ R/knitr-hooks.R
@@ -130,7 +130,7 @@ install_knitr_hooks <- function() {
         diagnostics <- as.numeric(options$exercise.diagnostics %||% 1 > 0)
         startover <- as.numeric(options$exercise.startover %||% 1 > 0)
         caption <- ifelse(is.null(options$exercise.cap), "Code", options$exercise.cap)
-        engine <- options$exercise.engine %||% ""
+        engine <- options$engine
         paste0('<div class="tutorial-', class,
                '" data-label="', options$label,
                '" data-engine="', engine,
