672ee6cc224c8873209abf466e7c8b7e1f631cf5
Winston Chang <winston@stdout.org>
2020-07-14 11:19:15 -0500

    Fix input handler id count, add docs

--- R/event.R
+++ R/event.R
@@ -1,29 +1,57 @@
 event_handlers <- new.env(parent = emptyenv())
 
-# Register an event handler on a per-tutorial basis. Handlers for an event will
-# be fired in the order that they were registered.
+#' Register an event handler callback
+#'
+#' Register an event handler on a per-tutorial basis. Handlers for an event will
+#' be fired in the order that they were registered. If
+#' `event_register_handler_once()` is used, the callback will execute only one
+#' time.
+#'
+#' @param event The name of an event.
+#' @param callback A function to be invoked when an event with a specified name
+#'   occurs. The callback must take parameters `session`, `event`, and `data`.
+#'
+#' @return A function which, if invoked, will remove the callback.
+#'
+#' @export
 event_register_handler <- function(event, callback) {
   if (!is.function(callback) ||
-      !all(names(formals(callback)) == c("session", "event", "data")))
+      !identical(names(formals(callback)), c("session", "event", "data")))
   {
     stop("`callback` must be a function that takes three arguments, `session`, `event`, and `data`.")
   }
 
   if (is.null(event_handlers[[event]])) {
     event_handlers[[event]] <- list()
-    last_id <- sprintf("%010d", 0)
-  } else {
-    last_id <- names(event_handlers[[event]])[[length(event_handlers[[event]])]]
+    attr(event_handlers[[event]], "last_id") <- 0
   }
 
+  id <- attr(event_handlers[[event]], "last_id", TRUE) + 1
+  attr(event_handlers[[event]], "last_id") <- id
+
   # IDs have names like "0000000001", "0000000002", "0000000003", etc.
-  id <- sprintf("%010d", as.numeric(last_id) + 1)
-  event_handlers[[event]][[id]] <- callback
+  id_str <- sprintf("%010d", id)
+  event_handlers[[event]][[id_str]] <- callback
 
   # Use this instead of a local anonymous function, so that we don't capture
   # `callback`, and other objects in the removal function, which might keep some
   # objects from getting GC'd.
-  invisible(create_event_handler_remover(event, id))
+  invisible(create_event_handler_remover(event, id_str))
+}
+
+#' @name event_register_handler
+#' @export
+event_register_handler_once <- function(event, callback) {
+  cancel_callback <- event_register_handler(
+    event,
+    function(session, event, data) {
+      # Use on.exit() to ensure cancellation in case callback throws an error.
+      on.exit(cancel_callback())
+      callback(session, event, data)
+    }
+  )
+
+  invisible(cancel_callback)
 }
 
 # Returns a function which removes an event handler.
