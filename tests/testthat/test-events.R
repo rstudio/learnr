49259722b858fa92883c85f2915177e16dec2624
Winston Chang <winston@stdout.org>
2020-07-14 11:22:30 -0500

    Add event handler tests

---
+++ tests/testthat/test-events.R
@@ -0,0 +1,81 @@
+test_that("Event handlers", {
+  # Check that session, event, data are passed to callback.
+  result <- NULL
+  cancel <- event_register_handler("foo",
+    function(session, event, data) { result <<- list(session, event, data) }
+  )
+  on.exit(cancel(), add = TRUE)
+  event_trigger("session_obj", "foo", "data")
+  expect_identical(result, list("session_obj", "foo", "data"))
+  cancel()
+
+
+  # Same as previous, but with event_register_handler_once.
+  result <- NULL
+  event_register_handler_once("foo",
+    function(session, event, data) { result <<- list(session, event, data) }
+  )
+  event_trigger("session_obj", "foo", "data")
+  expect_identical(result, list("session_obj", "foo", "data"))
+
+
+  # Testing multiple event handlers for same event, checking for order
+  x <- numeric()
+  cancel1 <- event_register_handler(
+    "foo",
+    function(session, event, data) { x <<- c(x, 1) }
+  )
+  on.exit(cancel1(), add = TRUE)
+
+  cancel2 <- event_register_handler_once(
+    "foo",
+    function(session, event, data) { x <<- c(x, 2) }
+  )
+  on.exit(cancel2(), add = TRUE)
+
+  cancel3 <- event_register_handler(
+    "foo",
+    function(session, event, data) { x <<- c(x, 3) }
+  )
+  on.exit(cancel3(), add = TRUE)
+
+  event_trigger(NULL, "foo", NA)
+  expect_identical(x, c(1, 2, 3))
+
+  event_trigger(NULL, "foo", NA)
+  expect_identical(x, c(1, 2, 3, 1, 3))
+
+  # Cancel first handler
+  expect_true(cancel1())
+  expect_false(cancel1())
+
+  event_trigger(NULL, "foo", NA)
+  expect_identical(x, c(1, 2, 3, 1, 3, 3))
+})
+
+
+test_that("Canceling one-time event handlers", {
+  n <- 0
+  cancel <- event_register_handler_once("foo", function(session, event, data) { n <<- n + 1 })
+
+  expect_true(cancel())
+  event_trigger(NULL, "foo", NA)
+  expect_identical(n, 0)
+  expect_false(cancel())
+})
+
+
+test_that("Event handler input checking", {
+  # Should error if callback has incorrect args (session, event, data)
+  expect_error(
+    event_register_handler("foo", function(session, data) NULL)
+  )
+  expect_error(
+    event_register_handler("foo", function(session, data, event) NULL)
+  )
+
+  # Error for empty event name
+  expect_error(
+    event_register_handler("", function(session, event, data) NULL)
+  )
+})
