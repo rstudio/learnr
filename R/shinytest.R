f4f761d6b436063d9dbcb6545b7ea4774f44c57f
Winston Chang <winston@stdout.org>
2020-07-31 16:29:20 -0500

    Use dput_to_string

--- R/shinytest.R
+++ R/shinytest.R
@@ -3,7 +3,7 @@ register_shinytest_inputprocessor <- function() {
     shinytest::registerInputProcessor("learnr.exercise", function(value) {
       # Drop all information from `value` except `code`.
       value <- value["code"]
-      paste(capture.output(dput(value)), collapse = "\n")
+      dput_to_string(value)
     })
   }
 }
