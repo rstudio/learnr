6666e5c9194c332d0e4f133836327b28effa26b3
Winston Chang <winston@stdout.org>
2020-07-31 12:23:50 -0500

    Make sure newlines are stored for shinytest

--- R/shinytest.R
+++ R/shinytest.R
@@ -2,6 +2,6 @@ register_shinytest_inputprocessor <- function() {
   shinytest::registerInputProcesser("learnr.exercise", function(value) {
     # Drop all information from `value` except `code`.
     value <- value["code"]
-    paste(capture.output(dput(value)), collapse = " ")
+    paste(capture.output(dput(value)), collapse = "\n")
   })
 }
