a09e71c509bae2db485037a1e391c5bd16d6273c
Winston Chang <winston@stdout.org>
2020-07-31 13:39:53 -0500

    Fix function name

--- R/shinytest.R
+++ R/shinytest.R
@@ -1,5 +1,5 @@
 register_shinytest_inputprocessor <- function() {
-  shinytest::registerInputProcesser("learnr.exercise", function(value) {
+  shinytest::registerInputProcessor("learnr.exercise", function(value) {
     # Drop all information from `value` except `code`.
     value <- value["code"]
     paste(capture.output(dput(value)), collapse = "\n")
