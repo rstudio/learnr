63aa19cc86f21f02a301f03785cb757b293b8652
Carson <cpsievert1@gmail.com>
2020-08-04 10:42:32 -0500

    Add news and comment

--- R/http-handlers.R
+++ R/http-handlers.R
@@ -184,6 +184,9 @@ register_http_handlers <- function(session, metadata) {
                       function.suffix = "(")
     on.exit(do.call(utils::rc.options, as.list(options)), add = TRUE)
 
+    # If and when exercises gain access to files, then we should evaluate this
+    # code in the exercise dir with `quotes = TRUE` (and sanitize to keep
+    # filename lookup local to exercise dir)
     settings <- utils::rc.settings()
     utils::rc.settings(ops = TRUE, ns = TRUE, args = TRUE, func = FALSE,
                        ipck = TRUE, S3 = TRUE, data = TRUE, help = TRUE,
