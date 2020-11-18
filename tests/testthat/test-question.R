3c32b7ec60005358361b2afd63f6b3d890a529b9
Garrick Aden-Buie <garrick@adenbuie.com>
2020-11-18 15:22:15 -0500

    Fixes loading placeholder when question text is HTML

--- tests/testthat/test-question.R
+++ tests/testthat/test-question.R
@@ -9,3 +9,15 @@ test_that("bad ellipses are found", {
     question("title", answer("5", correct = TRUE), typ = "auto")
   )
 })
+
+test_that("loading placeholder is correctly generated for HTML question texts", {
+  expect_silent(
+    q1 <- question(htmltools::tags$p("Did this work?"), answer("yes", correct = TRUE))
+  )
+
+  expect_silent(
+    q2 <- question(htmltools::HTML("<p>Did this work?</p>"), answer("yes", correct = TRUE))
+  )
+
+  expect_equal(q1$loading, q2$loading)
+})
