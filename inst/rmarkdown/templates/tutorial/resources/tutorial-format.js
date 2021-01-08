175fa1f8cb1b3581c4141913989122375d488820
Siebren Frölich <48289046+siebrenf@users.noreply.github.com>
2021-01-08 19:36:14 +0100

    add printButton

--- inst/rmarkdown/templates/tutorial/resources/tutorial-format.js
+++ inst/rmarkdown/templates/tutorial/resources/tutorial-format.js
@@ -350,6 +350,12 @@ $(document).ready(function() {
                         });
       });
       topicsFooter.append(resetButton);
+        
+      var printButton = $('<span class="printButton">Print</span>');
+      printButton.on('click', function() {
+        window.print();
+      });
+      topicsFooter.append(printButton);
       topicsList.append(topicsFooter);
 
       return topicsList;
