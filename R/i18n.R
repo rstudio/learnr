d765b69b849c96596f821728b6fb3bfc2507b7af
Alex Rossell Hayes <alexander@rossellhayes.com>
2021-07-12 12:53:10 -0700

    Refactor `i18n_combine_words()` to not use `glue`

--- R/i18n.R
+++ R/i18n.R
@@ -131,11 +131,13 @@ i18n_span <- function(key, ..., opts = NULL) {
 i18n_combine_words <- function(
   words, and = c("and", "or"), before = "", after = before
 ) {
-  and   <- match.arg(and)
-  sep   <- "$t(text.listcomma) "
-  last  <- glue::glue(" $t(text.{and}) ")
-  words <- glue::glue("{before}{words}{after}")
-  glue::glue_collapse(words, sep = sep, last = last)
+  and  <- match.arg(and)
+  last <- paste0(" $t(text.", and, ") ")
+  knitr::combine_words(
+    words,
+    sep = "$t(text.listcomma) ",
+    last = last, before = before, after = after, oxford_comma = FALSE
+  )
 }
 
 i18n_translations <- function() {
