4d51ff98e2a666be33ad77b3c760ba8a2c348f6d
Garrick Aden-Buie <garrick@adenbuie.com>
2021-12-03 10:57:04 -0500

    Make example cards more extensible

--- vignettes/articles/example_cards.R
+++ vignettes/articles/example_cards.R
@@ -1,10 +1,16 @@
-card <- function(...) {
+card <- function(
+  ...,
+  class_card = NULL,
+  class_card_body = NULL,
+  class_card_title = NULL,
+  class_card_footer = "text-end"
+) {
   card_data <- list(...)
   htmltools::withTags(
     div(
       class = "col",
       div(
-        class = "card h-100 shadow-sm",
+        class = c("card h-100 shadow-sm", class_card),
         a(
           href = card_data$link,
           img(
@@ -14,15 +20,15 @@ card <- function(...) {
           )
         ),
         div(
-          class = "card-body",
-          h5(class = "card-title", a(href = card_data$link, card_data$title)),
+          class = c("card-body", class_card_body),
+          h5(class = c("card-title", class_card_title), a(href = card_data$link, card_data$title)),
           if (!is.null(card_data$text)) div(
             class = "card-text text-muted fs-6",
             htmltools::HTML(commonmark::markdown_html(card_data$text))
           ),
         ),
         if (!is.null(card_data$footer)) div(
-          class = "card-footer text-end",
+          class = c("card-footer", class_card_footer),
           htmltools::HTML(card_data$footer)
         )
       )
@@ -30,18 +36,18 @@ card <- function(...) {
   )
 }
 
-example_cards <- function(yml, group = NULL, cols = "row-cols-1 row-cols-md-2 row-cols-lg-3") {
+example_cards <- function(yml, group = NULL, class_row = "row-cols-1 row-cols-md-2 row-cols-lg-3") {
   examples <- if (is.list(yml)) {
     yml
   } else {
     yaml::read_yaml(yml)
   }
 
-  examples <- purrr::keep(examples, function(x) x[["group"]] == group)
+  examples <- purrr::keep(examples, function(x) x[["group"]] %in% group)
   examples <- purrr::transpose(examples)
 
   htmltools::tags$div(
-    class = paste("row g-4", cols),
+    class = paste("row g-4", class_row),
     purrr::pmap(examples, card)
   )
 }
