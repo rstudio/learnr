1c01ac258230cbe217eee16c77cc71924faab1d3
Alex Rossell Hayes <44556601+rossellhayes@users.noreply.github.com>
2022-03-18 13:01:44 -0700

    Allow whitespace after code headers in `split_code_headers()` (#678)
    Co-authored-by: Garrick Aden-Buie <garrick@adenbuie.com>

---
+++ tests/testthat/test-staticimports.R
@@ -0,0 +1,37 @@
+test_that("split_code_headers()", {
+  target <- list(one = "1", two = "2")
+
+  # No whitespace after dashes
+  expect_equal(
+    split_code_headers(
+"# one ----
+1
+# two ----
+2"
+    ),
+    target
+  )
+
+
+  # Whitespace after first header
+  expect_equal(
+    split_code_headers(
+"# one ----
+1
+# two ----
+2"
+    ),
+  target
+  )
+
+  # Whitespace after subsequent headers
+  expect_equal(
+    split_code_headers(
+"# one ----
+1
+# two ----
+2"
+    ),
+  target
+)
+})
