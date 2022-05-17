d4a4d6996532266b475c21f3176c088e57065622
Garrick Aden-Buie <garrick@adenbuie.com>
2022-05-17 12:53:35 -0400

    warm-up chromote

---
+++ tests/testthat/test-shinytest2-aaa.R
@@ -0,0 +1,15 @@
+# https://github.com/rstudio/shinytest2/blob/c29b78e9/tests/testthat/test-aaa.R
+skip_on_cran() # Uses chromote
+
+# Try to warm up chromote. IDK why it fails on older versions of R.
+test_that("Chromote loads", {
+  skip_if(!on_ci(), "Not on CI")
+
+  # Wrap in a `try()` as the test doesn't matter
+  # Only the action of trying to open chromote matters
+  try({
+    chromote::default_chromote_object()$new_session()
+  })
+
+  expect_true(TRUE)
+})
