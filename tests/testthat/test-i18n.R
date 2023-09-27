f3c157e63f386d40c72ee17d5dbe3bb23247896b
Garrick Aden-Buie <garrick@adenbuie.com>
2023-09-27 18:55:35 -0400

    tests(i18n): Skip also if LANG is C/C.UTF-8

--- tests/testthat/test-i18n.R
+++ tests/testthat/test-i18n.R
@@ -205,7 +205,7 @@ test_that("i18n_span() returns an i18n span", {
 
 test_that("i18n_set_language_option() changes message language", {
   skip_if_not_pandoc("1.14")
-  skip_if_lc_all_is_c()
+  skip_if_lang_is_c()
 
   withr::defer(i18n_set_language_option("en"))
 
@@ -235,7 +235,7 @@ test_that("i18n_set_language_option() changes message language", {
 
 test_that("i18n_set_language_option() sets up language inheritance", {
   skip_if_not_pandoc("1.14")
-  skip_if_lc_all_is_c()
+  skip_if_lang_is_c()
 
   withr::defer(i18n_set_language_option("en"))
 
